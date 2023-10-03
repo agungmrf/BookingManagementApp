09379ea029e3f481201a18758966873c5f6b485a
Agung Ma'ruf <1911500518@student.budiluhur.ac.id>
2023-10-03 12:13:02 +0700

    updated: add Guid & Password Account

--- API/DTOs/Accounts/AccountDto.cs
+++ API/DTOs/Accounts/AccountDto.cs
@@ -5,26 +5,29 @@ namespace API.DTOs.Accounts;
 public class AccountDto
 {
     public Guid Guid { get; set; }
+    public string Password { get; set; }
     public int Otp { get; set; }
     public bool IsUsed { get; set; }
     public DateTime ExpiredDate { get; set; }
     
-    public static explicit operator AccountDto(Account account)
+    public static explicit operator AccountDto(Account account) // Operator explicit untuk mengkonversi Account menjadi AccountDto.
     {
-        return new AccountDto
+        return new AccountDto // Mengembalikan object AccountDto dengan data dari property Account.
         {
             Guid = account.Guid,
+            Password = account.Password,
             Otp = account.Otp,
             IsUsed = account.IsUsed,
             ExpiredDate = account.ExpiredDate
         };
     }
     
-    public static implicit operator Account(AccountDto accountDto)
+    public static implicit operator Account(AccountDto accountDto) // Operator implicit untuk mengkonversi AccountDto menjadi Account.
     {
-        return new Account
+        return new Account // Mengembalikan object Account dengan data dari property AccountDto.
         {
             Guid = accountDto.Guid,
+            Password = accountDto.Password,
             Otp = accountDto.Otp,
             IsUsed = accountDto.IsUsed,
             ExpiredDate = accountDto.ExpiredDate,
--- API/DTOs/Accounts/CreateAccountDto.cs
+++ API/DTOs/Accounts/CreateAccountDto.cs
@@ -4,15 +4,17 @@ namespace API.DTOs.Accounts;
 
 public class CreateAccountDto
 {
+    public Guid Guid { get; set; }
     public string Password { get; set; }
     public int Otp { get; set; }
     public bool IsUsed { get; set; }
     public DateTime ExpiredDate { get; set; }
 
-    public static implicit operator Account(CreateAccountDto createAccountDto)
+    public static implicit operator Account(CreateAccountDto createAccountDto) // Operator implicit untuk mengkonversi CreateAccountDto menjadi Account.
     {
-        return new Account
+        return new Account // Mengembalikan object Account dengan data dari property CreateAccountDto.
         {
+            Guid = createAccountDto.Guid,
             Password = createAccountDto.Password,
             Otp = createAccountDto.Otp,
             IsUsed = createAccountDto.IsUsed,
